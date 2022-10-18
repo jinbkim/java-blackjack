82404b5f677519198c15424aae387ed28d325880
kjb95 <jinbkim42@gmail.com>
2022-10-18 15:23:44 +0900

    feat: 각 플레이어별 배팅 금액 비정상적인 입력받기
    - 숫자가 아닌 경우
    - 0원 이하인 경우

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -3,9 +3,11 @@ package blackjack.view;
 import blackjack.model.PlayerBetAmounts;
 import blackjack.model.PlayerNames;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class InputView {
 
+    private static String ONLY_NUMBER_REGEX = "^[0-9]*$";
 
     public PlayerNames requestPlayerName() {
         String input = requestInput(OutputView.REQUEST_PLAYER_NAME);
@@ -21,15 +23,30 @@ public class InputView {
     public PlayerBetAmounts requestPlayerBetAmounts(PlayerNames playerNames) {
         PlayerBetAmounts playerBetAmounts = new PlayerBetAmounts();
 
-        playerNames.getNames()
-            .stream()
-            .forEach(name -> playerBetAmounts.add(requestPlayerBetAmount(name)));
+        for (String name : playerNames.getNames()) {
+            playerBetAmounts.add(requestPlayerBetAmount(name));
+        }
         return playerBetAmounts;
     }
 
-    private int requestPlayerBetAmount(String PlayerName) {
-        String input = requestInput(PlayerName + OutputView.REQUEST_BET_AMOUNT);
+    public void validateBetAmount(String input) {
+        if (!Pattern.matches(ONLY_NUMBER_REGEX, input)) {
+            throw new IllegalArgumentException(OutputView.WRONG_BET_AMOUNT);
+        }
+        if (Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException(OutputView.WRONG_BET_AMOUNT);
+        }
+    }
 
+    private int requestPlayerBetAmount(String playerName) {
+        String input = requestInput(playerName + OutputView.REQUEST_BET_AMOUNT);
+
+        try {
+            validateBetAmount(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return requestPlayerBetAmount(playerName);
+        }
         return Integer.parseInt(input);
     }
 
---
+++ src/test/java/blackjack/view/InputViewTest.java
@@ -0,0 +1,17 @@
+package blackjack.view;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+
+    private InputView inputView = new InputView();
+
+    @Test
+    void 배팅_금액_비정상적인_입력() {
+        Assertions.assertThatThrownBy(() -> inputView.validateBetAmount("a"));
+        Assertions.assertThatThrownBy(() -> inputView.validateBetAmount("1b"));
+        Assertions.assertThatThrownBy(() -> inputView.validateBetAmount("-3"));
+        Assertions.assertThatThrownBy(() -> inputView.validateBetAmount("0"));
+    }
+}
