caa049c7490376520fcd3bc3f0a7ee54922997fe
kjb95 <jinbkim42@gmail.com>
2022-10-18 13:23:29 +0900

    feat: 각 플레이어별 배팅 금액 정상적인 입력받기

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -1,12 +1,14 @@
 package blackjack.controller;
 
 import blackjack.view.InputView;
+import java.util.List;
 
 public class GameController {
 
     final InputView inputView = new InputView();
 
     public void run() {
-        inputView.requestPlayer();
+        List<String> playersName = inputView.requestPlayerName();
+        List<Integer> playersBetAmount = inputView.requestPlayersBetAmount(playersName);
     }
 }
--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,21 +1,60 @@
 package blackjack.view;
 
-import blackjack.model.Players;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    public Players requestPlayer() {
-        String input = requestInput(OutputView.REQUEST_PLAYER);
+    private static final String SPACE_REGEX = "\\s";
+    private static final String PLAYER_SEP_REGEX = ",";
+
+    public List<String> requestPlayerName() {
+        String input = requestInput(OutputView.REQUEST_PLAYER_NAME);
+
         try {
-            return new Players(input);
+            return parseRequestPlayerName(input);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return requestPlayer();
+            return requestPlayerName();
         }
     }
 
-    public String requestInput(String input) {
+    public List<Integer> requestPlayersBetAmount(List<String> playersName) {
+        List<Integer> playersBetAmount = new ArrayList<>();
+
+        for (String playerName : playersName) {
+            int betAmount = requestPlayerBetAmount(playerName);
+            playersBetAmount.add(betAmount);
+        }
+        return playersBetAmount;
+    }
+
+    private int requestPlayerBetAmount(String PlayerName) {
+        String input = requestInput(PlayerName + OutputView.REQUEST_BET_AMOUNT);
+
+        return Integer.parseInt(input);
+    }
+
+    private List<String> parseRequestPlayerName(String input) {
+        String noSpaceInput = input.replaceAll(SPACE_REGEX, "");
+        List<String> playersName = Arrays.asList(noSpaceInput.split(PLAYER_SEP_REGEX));
+
+        if (hasPlayerEmptyName(playersName)) {
+            throw new IllegalArgumentException(OutputView.WRONG_PLAYER_NAME);
+        }
+        return playersName;
+    }
+
+    private boolean hasPlayerEmptyName(List<String> playersName) {
+        return playersName.stream()
+            .filter(name -> name.length() == 0)
+            .count() != 0;
+    }
+
+
+    private String requestInput(String input) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(input);
