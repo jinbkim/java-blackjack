6e8d632c5a9f30dee98c67dd2a5356422bbb7b2c
kjb95 <topmedia44@gmail.com>
2022-10-20 17:06:44 +0900

    feat: 게임 시작시, 플레이어들은 랜덤의 두 장의 카드 받기

--- src/main/java/blackjack/model/player/Player.java
+++ src/main/java/blackjack/model/player/Player.java
@@ -1,9 +1,12 @@
 package blackjack.model.player;
 
+import blackjack.model.card.Cards;
+
 public class Player {
 
-    private String name;
+    private final String name;
     private int money;
+    private Cards cards = new Cards();
 
     public Player(String name, int money) {
         this.name = name;
